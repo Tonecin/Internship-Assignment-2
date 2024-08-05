335f59cbff904a43d69cb2978ff4aba25dc085bf
HOANG TUAN MINH <132455528+Tonecin@users.noreply.github.com>
2024-08-05 16:58:43 +0800

    Add files via upload

---
+++ src/components/Actor.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <span>
+        <button @click="deleteActor">Delete</button>
+      </span>
+      <span>
+        {{ actor }} 
+      </span>
+    </div>
+</template>
+
+<script>
+
+export default {
+    emits: ['deleteActor'],
+    props: {
+        actor: {
+            type: String,
+            required: true
+        }
+    },
+    setup(props, { emit }) {
+
+        const deleteActor = () => {
+            emit('deleteActor', props.actor)
+        }
+
+        return {
+            deleteActor,
+            actor: props.actor
+        };
+    }
+}
+</script>
+
+<style scoped>
+div {
+    padding-left: 0.5cm;
+}
+</style>
